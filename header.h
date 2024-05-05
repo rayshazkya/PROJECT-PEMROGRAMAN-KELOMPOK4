82664c6ad0525ac30aa55b82ac1d6f00751c4df2
shairafathiya <167387931+shairafathiya@users.noreply.github.com>
2024-05-05 17:54:55 +0700

    Update header.h

--- header.h
+++ header.h
@@ -5,3 +5,7 @@
 void main_menu_admin ();
 char Login_user (  char username [] ,  char password[] );
 char Login_Admin (  char username [] ,  char password[] );
+void main_menu_admin ();
+void main_menu_user();
+void kembali_main_admin(char back);
+void kembali_main_user(char back);
