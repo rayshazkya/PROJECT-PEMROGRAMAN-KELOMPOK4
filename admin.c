624429caaf8d51511079758534722535afd102fc
rayshazkya <167387273+rayshazkya@users.noreply.github.com>
2024-05-07 22:21:11 +0700

    Update admin.c

--- admin.c
+++ admin.c
@@ -3,28 +3,28 @@
 #include <string.h>
 #include "header.h"
 
-#define MAX_BUKU 100 // Maksimum jumlah buku yang dapat disimpan
+#define MAX_BUKU 100 // jumlah maksimal buku yang dapat di input
 
-// Fungsi untuk menyimpan data buku ke dalam file
+// Fungsi untuk menyimpan data
 void simpanKeFile(struct Buku buku[], int jumlahBuku) {
     FILE *file = fopen("databuku.txt", "w");
     if (file == NULL) { // Memeriksa apakah file berhasil dibuka
         printf("Gagal membuka file.\n");
         return;
     }
-    // Menulis data buku ke dalam file
+    // menulis data buku dalam file
     for (int i = 0; i < jumlahBuku; i++) {
         fprintf(file, "%d %s %s %s %d %d %d\n", buku[i].id, buku[i].judul, buku[i].penulis, buku[i].penerbit, buku[i].jumlah_halaman, buku[i].tahun_terbit, buku[i].jumlah_tersedia);
     }
-    fclose(file); // Menutup file setelah selesai
+    fclose(file); 
 }
 
 int main() {
-    struct Buku buku[MAX_BUKU]; // Array untuk menyimpan data buku
+    struct Buku buku[MAX_BUKU]; 
     int jumlahBuku = 0;
     FILE *file = fopen("databuku.txt", "r");
     if (file != NULL) {
-        // Membaca data buku dari file jika ada
+        // Membaca data buku dari file 
         while (fscanf(file, "%d %s %s %s %d %d %d", &buku[jumlahBuku].id, buku[jumlahBuku].judul, buku[jumlahBuku].penulis, buku[jumlahBuku].penerbit, &buku[jumlahBuku].jumlah_halaman, &buku[jumlahBuku].tahun_terbit, &buku[jumlahBuku].jumlah_tersedia) != EOF) {
             jumlahBuku++;
         }
@@ -38,7 +38,7 @@ int main() {
 
     // Menu utama
      do {
-        choice = tampilkanMenu(); // Memanggil fungsi untuk menampilkan menu
+        choice = tampilkanMenu(); // Memanggil fungsi menu
         switch (choice) {
             case 1:
                 tambahBuku(buku, &jumlahBuku);
@@ -54,7 +54,7 @@ int main() {
                 editBuku(buku, &jumlahBuku, id);
                 break;
             case 4:
-                simpanKeFile(buku, jumlahBuku); // Menyimpan data buku ke dalam file sebelum keluar
+                simpanKeFile(buku, jumlahBuku); //data buku disimpan ke dalam file sebelum keluar
                 printf("Terima kasih telah menggunakan program admin buku.\n");
                 break;
             default:
