81aac966cd513d139a4d8fa775b79252a73cfe4b
rayshazkya <167387273+rayshazkya@users.noreply.github.com>
2024-05-07 23:05:30 +0700

    Update header.h

--- header.h
+++ header.h
@@ -16,7 +16,7 @@ struct Buku {
 int tampilkanMenu();
 
 //Deklarasi fungsi file admin.c
-void tambahBuku(struct Buku buku[], int *jumlahBuku);
-void hapusBuku(struct Buku buku[], int *jumlahBuku, int id);
-void editBuku(struct Buku buku[], int *jumlahBuku, int id);
-void simpanKeFile(struct Buku buku[], int jumlahBuku);
+void tambahBuku();
+void hapusBuku();
+void editBuku();
+void simpanKeFile();
