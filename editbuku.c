f7983cafc695f539e3cfba0b81f69a5351afcd71
shairafathiya <167387931+shairafathiya@users.noreply.github.com>
2024-05-12 22:49:16 +0700

    Update editbuku.c

--- editbuku.c
+++ editbuku.c
@@ -1,38 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include "header.h"
 
-// Fungsi mengedit data buku berdasarkan ID
-void editBuku(){
-struct Buku buku[];
-int *jumlahBuku, int id;
-    int i, found = 0;
-    // Mencari buku dengan ID yang sesuai
-    for (i = 0; i < *jumlahBuku; i++) {
-        if (buku[i].id == id) {
-            found = 1;
-            break;
+// fungsi edit buku
+void editBuku() {
+    FILE *file = fopen("databuku.txt", "r");
+    FILE *temp = fopen("tempfile.txt", "w");
+    if (file == NULL || temp== NULL){
+        printf("Eror Mebuka file!");
+        exit (EXIT_FAILURE);
+    }
+    
+    int edit;
+    int id = 1000;
+    Buku buku[MAX_BOOKS];
+    int i=0;
+    int *jumlah_buku;
+    
+    printf("Masukkan ID buku yang ingin di edit: ");
+    scanf("%d", &edit);
+    
+
+    char line[1024];
+    
+    while (fgets(line, 1024, file) != NULL) {
+        id++;
+        if (id != edit) {
+            fprintf(temp, "%s", line); //jika id tidak sama maka akan di print ke temp
         }
-    }
-    // jika buku ditemukan 
-    if (found) {
-        // Meminta user untuk memasukkan data baru
-        printf("Masukkan judul baru: ");
-        scanf("%s", buku[i].judul);
-        printf("Masukkan penulis baru: ");
-        scanf("%s", buku[i].penulis);
-        printf("Masukkan penerbit baru: ");
-        scanf("%s", buku[i].penerbit);
-        printf("Masukkan jumlah halaman baru: ");
-        scanf("%d", &buku[i].jumlah_halaman);
-        printf("Masukkan tahun terbit baru: ");
-        scanf("%d", &buku[i].tahun_terbit);
-        printf("Masukkan jumlah buku yang tersedia: ");
-        scanf("%d", &buku[i].jumlah_tersedia);
         
-        printf("Data buku dengan ID %d berhasil diubah.\n", id);
-    } else {
-        printf("Buku dengan ID %d tidak ditemukan.\n", id);
+        i++; //jika id sama , maka program memninta user memasukkan data buku 
+        if (id == edit) {
+            
+            printf("\n--EDIT BUKU--\n");
+            printf("ID buku: ");
+            scanf("%d", &buku[i].id);
+            getchar();
+            printf("\nJudul: ");
+            scanf("%[^\n]", buku[i].judul);
+            getchar();
+            printf("\nPenulis: ");
+            scanf("%[^\n]", buku[i].penulis);
+            getchar();
+            printf("\nPenerbit: ");
+            scanf("%[^\n]", buku[i].penerbit);
+            getchar();
+            printf("\nJumlah halaman: ");
+            scanf("%u", &buku[i].jumlah_halaman);
+            getchar();
+            printf("\nTahun terbit: ");
+            scanf("%u", &buku[i].tahun_terbit);
+            getchar();
+            printf("\nJumlah buku tersedia: ");
+            scanf("%u", &buku[i].jumlah_buku_tersedia);
+            getchar();
+            
+            fprintf(temp, "%d %s %s %s %u %u %u\n", buku[i].id, buku[i].judul, buku[i].penulis, buku[i].penerbit, buku[i].jumlah_halaman, buku[i].tahun_terbit, buku[i].jumlah_buku_tersedia);
+        } 
     }
+  
+    fclose(file);
+    fclose(temp);
+
+     
+    // databuku yang lama di hapus , kemudian di rename ,jdi tempfile= databuku baru
+
+   if (remove("databuku.txt") != 0) {
+        printf("Error deleting original file!\n");
+        exit (EXIT_FAILURE);
+    }
+
+    if (rename("tempfile.txt", "databuku.txt") != 0) {
+        printf("Error renaming temporary file!\n");
+        exit (EXIT_FAILURE);
+    }
+   
+
+    printf("BERHASIL!\n");
+    main_menu_admin();
 }
+
+   
+
+
