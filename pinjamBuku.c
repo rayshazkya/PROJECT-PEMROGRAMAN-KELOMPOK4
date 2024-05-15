d9e9c835c876622adb5acce668305b2c50ded30c
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-05-16 01:26:56 +0700

    Update pinjamBuku.c

--- pinjamBuku.c
+++ pinjamBuku.c
@@ -1,11 +1,36 @@
 
 #include "header.h"
 
+// Fungsi untuk membaca data buku dari file
+int bacaDataBuku(Buku *daftar_buku) {
+    FILE *file = fopen("databuku.txt", "r");
+    if (file == NULL) {
+        printf("Gagal membuka file databuku.txt\n");
+        exit(1);
+    }
 
-// Fungsi untuk meminjam buku
+    int jumlah_buku = 0;
+    while (fscanf(file, "%u \"%99[^\"]\" \"%99[^\"]\" \"%99[^\"]\" %u %u %u\n", 
+                    &daftar_buku[jumlah_buku].id, 
+                    daftar_buku[jumlah_buku].judul, 
+                    daftar_buku[jumlah_buku].penulis, 
+                    daftar_buku[jumlah_buku].penerbit, 
+                    &daftar_buku[jumlah_buku].jumlah_halaman, 
+                    &daftar_buku[jumlah_buku].tahun_terbit, 
+                    &daftar_buku[jumlah_buku].jumlah_buku_tersedia) == 7) {
+
+        jumlah_buku++;
+    }
+
+    fclose(file);
+    return jumlah_buku;
+}
+
 void pinjamBuku(Buku *daftar_buku, int jumlah_buku, int id_user, unsigned int id) {
     int k = -1;
+    FILE *files = fopen("databukusemntr.txt", "w");
 
+    // Cari buku berdasarkan ID
     for (int i = 0; i < jumlah_buku; i++) {
         if (daftar_buku[i].id == id) {
             k = i;
@@ -19,7 +44,17 @@ void pinjamBuku(Buku *daftar_buku, int jumlah_buku, int id_user, unsigned int id
     }
 
     if (daftar_buku[k].jumlah_buku_tersedia > 0) {
+        for(int i =0; i < jumlah_buku; i++){
         daftar_buku[k].jumlah_buku_tersedia--;
+        fprintf(files,"%u \"%s %s %s %u %u %u\n", daftar_buku[i].id, 
+                    daftar_buku[i].judul, 
+                    daftar_buku[i].penulis, 
+                    daftar_buku[i].penerbit, 
+                    daftar_buku[i].jumlah_halaman, 
+                    daftar_buku[i].tahun_terbit, 
+                    daftar_buku[i].jumlah_buku_tersedia);
+        }
+
         printf("Buku '%s' berhasil dipinjam.\n", daftar_buku[k].judul);
 
         // Simpan data peminjaman buku
@@ -36,9 +71,8 @@ void pinjamBuku(Buku *daftar_buku, int jumlah_buku, int id_user, unsigned int id
         }
         fprintf(file, "%d,%u,%s\n", buku_dipinjam.id_user, buku_dipinjam.id_buku, buku_dipinjam.judul);
         fclose(file);
-        main_menu_user();
-
     } else {
         printf("Maaf, buku '%s' sedang tidak tersedia.\n", daftar_buku[k].judul);
     }
+    fclose(files);
 }
